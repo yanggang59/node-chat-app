827bad4bc69cbac9de1e3a1e7b63a7172d73f51f
yanggang59 <1449381582@qq.com>
2018-11-21 16:02:19 +0800

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -20,6 +20,22 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
+  //socket.emit from admin text welcome to the chat app
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:'Welcome to the chat app'
+  });
+
+
+  //socket.broadcast.emit from admin text:New user joined
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'New user joined',
+    createdAt:new Date().getTime()
+  });
+
+
+
   socket.on('createMessage',(message)=>{
     console.log('createMessage: ',message);
     io.emit('newMessage',{
@@ -27,6 +43,13 @@ io.on('connection',(socket)=>{
       text:message.text,
       createdAt:new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage',{
+    //     form:message.from,
+    //     text:message.text,
+    //     createdAt:new Date().getTime()
+    // });
+
   });
 
   socket.on('disconnect',()=>{
