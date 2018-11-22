0e33dd262108d657e149b0f8ac38deb56f46cd81
yanggang59 <1449381582@qq.com>
2018-11-22 17:49:42 +0800

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,24 @@
 var socket = io();
+
+function scrollToBottom(){
+  //Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    //console.log("should scroll");
+    messages.scrollTop(scrollHeight);
+  }
+
+};
+
+
 socket.on('connect',function (){
   console.log('connected to server');
 });
@@ -17,6 +37,7 @@ socket.on('newMessage',function (message){
     createdAt:formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 
@@ -29,13 +50,12 @@ socket.on('newLocationMessage',function(message){
     url:message.url
   });
    jQuery('#messages').append(html);
+   scrollToBottom();
 });
 
 jQuery('#message-form').on('submit',function(e){
   e.preventDefault();
-
   var messageTextbox = jQuery('[name=message]');
-
   socket.emit('createMessage',{
     from:'User',
     text:messageTextbox.val()
