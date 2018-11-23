a0b944b48bac5c6af7698f164ae734630d13ba35
yanggang59 <1449381582@qq.com>
2018-11-23 17:59:00 +0800

    Send messages to only people in the same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -79,7 +79,6 @@ jQuery('#message-form').on('submit',function(e){
   e.preventDefault();
   var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage',{
-    from:'User',
     text:messageTextbox.val()
   },function(){
     messageTextbox.val('');
